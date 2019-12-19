e70e0d04019c27c4d1dade9950f882dfecc7abaf
Oliver Eikemeier <eikemeier@fillmore-labs.com>
2019-12-19 11:45:52 +0100

    Check for single-record format marker

--- src/test/java/de/melsicon/kafka/serialization/avro/SerializationTest.java
+++ src/test/java/de/melsicon/kafka/serialization/avro/SerializationTest.java
@@ -20,6 +20,11 @@ public final class SerializationTest {
         SensorState.newBuilder().setId("7331").setTime(INSTANT).setState(State.OFF).build();
 
     var encoded = SensorState.getEncoder().encode(sensorState);
+
+    // Check for single-record format marker
+    // http://avro.apache.org/docs/1.9.1/spec.html#single_object_encoding
+    assertThat(encoded.getShort(0)).isEqualTo((short) 0xc301);
+
     var decoded = SensorState.getDecoder().decode(encoded);
 
     assertThat(decoded).isEqualToComparingFieldByField(sensorState);
