ac2e903e3c22220e7cddc2aaef7473b402f7311d
Oliver Eikemeier <eikemeier@fillmore-labs.com>
2020-03-25 18:14:55 +0100

    Update Jackson

--- src/main/java/de/melsicon/kafka/serde/json/JsonHelper.java
+++ src/main/java/de/melsicon/kafka/serde/json/JsonHelper.java
@@ -3,6 +3,7 @@ package de.melsicon.kafka.serde.json;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -13,16 +14,15 @@ public final class JsonHelper {
   private JsonHelper() {}
 
   public static ObjectMapper mapper() {
-    return new ObjectMapper()
-        .registerModule(new Jdk8Module())
-        .registerModule(new JavaTimeModule())
-        .registerModule(new GuavaModule())
+    return JsonMapper.builder()
+        .addModules(new Jdk8Module(), new JavaTimeModule(), new GuavaModule())
         .configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, /* state= */ true)
         .configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, /* state= */ true)
         .addMixIn(SensorState.class, SensorStateMixIn.class)
         .addMixIn(SensorState.Builder.class, SensorStateMixIn.BuilderMixIn.class)
         .addMixIn(SensorStateWithDuration.class, SensorStateWithDurationMixIn.class)
         .addMixIn(
-            SensorStateWithDuration.Builder.class, SensorStateWithDurationMixIn.BuilderMixIn.class);
+            SensorStateWithDuration.Builder.class, SensorStateWithDurationMixIn.BuilderMixIn.class)
+        .build();
   }
 }
